a0a3ff8cc9074b30a34a1d16d7ed02a53fd55108
sreenivas-git <sudhaadvaitha0722@gmail.com@gmail.com>
2023-09-05 23:44:57 -0700

    added test case2 in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,8 @@ test("renders learn react link", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+t("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
